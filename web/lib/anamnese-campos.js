88c6e1ea730297f0390a4ebc01729c8551eb103e
carlosventto-afk <carlosventto@gmail.com>
2026-08-15 11:45:01 -0300

    feat: adiciona lista compartilhada dos campos de anamnese

---
+++ web/lib/anamnese-campos.js
@@ -0,0 +1,13 @@
+export const CAMPOS_ANAMNESE = [
+  { chave: "medicacao_em_uso", rotulo: "Medicação em uso" },
+  { chave: "medico_responsavel", rotulo: "Médico responsável" },
+  { chave: "terapia_desde", rotulo: "Desde quando faz terapia" },
+  { chave: "atendido_desde", rotulo: "Desde quando é atendido por este profissional" },
+  { chave: "queixa_inicial", rotulo: "Queixa inicial" },
+  { chave: "desenvolvimento_queixa", rotulo: "Desenvolvimento da queixa" },
+  { chave: "historico_familiar", rotulo: "Histórico familiar relevante" },
+  { chave: "tratamento_anterior", rotulo: "Tratamento psicológico/psiquiátrico anterior" },
+  { chave: "uso_substancias", rotulo: "Uso de substâncias" },
+  { chave: "hipotese_diagnostica", rotulo: "Hipótese diagnóstica / comorbidades" },
+  { chave: "expectativas", rotulo: "Expectativas com o processo terapêutico" },
+];
