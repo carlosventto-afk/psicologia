50e793f997815c113410a30a6ec661427376e1dd
carlosventto-afk <carlosventto@gmail.com>
2026-08-25 18:19:40 -0300

    feat: adiciona lista compartilhada dos campos de completar cadastro

---
+++ web/lib/completar-cadastro-campos.js
@@ -0,0 +1,9 @@
+export const CAMPOS_COMPLETAR_CADASTRO = [
+  { chave: "telefone", rotulo: "Telefone", tipo: "text" },
+  { chave: "email", rotulo: "E-mail", tipo: "email" },
+  { chave: "endereco", rotulo: "Endereço", tipo: "text" },
+  { chave: "cpf", rotulo: "CPF", tipo: "text" },
+  { chave: "rg_numero", rotulo: "Número do RG", tipo: "text" },
+  { chave: "rg_data_expedicao", rotulo: "Data de expedição do RG", tipo: "date" },
+  { chave: "rg_orgao_emissor", rotulo: "Órgão emissor do RG", tipo: "text" },
+];
